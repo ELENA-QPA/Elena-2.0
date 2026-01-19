4562198225084168bd50ac2d84306429754a3a7c
SimonRamos88 <joramos@unal.edu.co>
2026-01-19 15:48:31 -0500

    try catch en main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,39 +4,44 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.setGlobalPrefix('api');
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.setGlobalPrefix('api');
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      // whitelist: true,
-      // forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
-  const config = new DocumentBuilder()
-    .setTitle('QP Alliance API')
-    .setDescription('QP Alliance App Endpoints')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .addApiKey(
-      {
-        type: 'apiKey',
-        name: 'x-api-key',
-        in: 'header',
-        description: 'API Key for external access',
-      },
-      'api-key',
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT || 8080;
-  await app.listen(port, '0.0.0.0');
+    app.useGlobalPipes(
+      new ValidationPipe({
+        // whitelist: true,
+        // forbidNonWhitelisted: true,
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    const config = new DocumentBuilder()
+      .setTitle('QP Alliance API')
+      .setDescription('QP Alliance App Endpoints')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addApiKey(
+        {
+          type: 'apiKey',
+          name: 'x-api-key',
+          in: 'header',
+          description: 'API Key for external access',
+        },
+        'api-key',
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    const port = process.env.PORT || 8080;
+    await app.listen(port, '0.0.0.0');
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+    throw new BadRequestException('Failed to start application');
+  }
 }
 
 bootstrap();
